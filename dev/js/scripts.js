47c0863f35b639af324015f843e6e6a634389635
Esther Lee <el686@drexel.edu>
2023-05-25 08:05:26 -0400

    navigation

---
+++ dev/js/scripts.js
@@ -0,0 +1,22 @@
+let canSeeMenu;
+let burgerBtn = document.querySelector("#burger-btn");
+let mainMenu = document.querySelector("#main-menu");
+
+canSeeMenu = false;
+
+burgerBtn.addEventListener("click", () => {
+    console.log(canSeeMenu);
+
+    if (!canSeeMenu) {
+        mainMenu.classList.add("show-menu");
+        canSeeMenu = true;
+    } else {
+        mainMenu.classList.remove("show-menu");
+        canSeeMenu = false;
+    }
+});
+
+mainMenu.addEventListener("click", () => {
+    mainMenu.classList.remove("show-menu");
+    canSeeMenu = false;
+});
